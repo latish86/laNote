67c8ce440c7c0bc2b58f6623c6b3c076ad154432
latish86 <latish86@bk.ru>
2018-10-05 07:43:58 +0500

    Add VueGulp url: https://laravel.ru/posts/352#uw352-создание-5

---
+++ frontend/resources/assets/js/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile">
+    {{ name }}
+    dasfsadf
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'Matt dsStauffer'
+    };
+  }
+};
+</script>
+
+<style>
+.profile {
+    background: #eee;
+    border: 1px solid #aaa;
+    border-radius: 2em;
+    margin: 2em auto;
+    min-height: 150px;
+    padding: 2em;
+    width: 300px;
+}
+</style>
