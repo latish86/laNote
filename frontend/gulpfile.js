14a2e40d1e84fdc3449fa1cde72cf3f53ed5b510
latish86 <latish86@bk.ru>
2018-10-05 08:09:05 +0500

    Go Work

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,5 +3,14 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-vueify');
 
 elixir(function(mix) {
+    mix.sass('main.scss');
     mix.browserify('app.js');
+    mix.browserSync({
+        server: {
+            baseDir: "./public",
+            // directory: true,
+            // index: "index.html"
+        },
+        proxy: "localhost:3000"
+    });
 });
